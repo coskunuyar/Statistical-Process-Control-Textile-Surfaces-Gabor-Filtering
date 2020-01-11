c7c4a40d5d810c0ad81d2b7ba12eb6bd3daa3be1
Coskun Uyar <coskunuyarr@gmail.com>
2020-01-11 17:27:28 +0300

    initial deploy

---
+++ files/Image18.R
@@ -0,0 +1,44 @@
+library(jpeg)
+library(wvtool)
+library("matrixStats")
+imgActual <- readJPEG("C:/Fabric18-ConvertImage.jpg")
+img1 <- readJPEG("C:/Fabric18-ConvertImage.jpg")
+
+gaborFilter <- gabor.filter(x=imgActual, lamda=15, theta=30, bw=5, phi=0, asp=0.7, disp=TRUE)
+imgGabor <-gaborFilter$filtered_img
+
+# Column Gabor
+mean_col_Gabor <- colMeans(imgGabor)
+std_col_Gabor <- colSds(imgGabor)
+control_limits_col_Gabor <- matrix(,512,3)
+control_limits_col_Gabor[1:512,1] = mean_col_Gabor[1:512]-(3)*std_col_Gabor[1:512]
+control_limits_col_Gabor[1:512,2] = mean_col_Gabor[1:512]
+control_limits_col_Gabor[1:512,3] = mean_col_Gabor[1:512]+(3)*std_col_Gabor[1:512]
+for(i in 1:512){
+  for(k in 1:512){
+    if(imgGabor[i,k] < control_limits_col_Gabor[i,1] | imgGabor[i,k] > control_limits_col_Gabor[i,3] ){
+      img1[i,k] = 0;
+    } 
+  }
+}
+# Row Gabor
+mean_row_Gabor <- rowMeans(imgGabor)
+std_row_Gabor <- rowSds(imgGabor)
+control_limits_row_Gabor <- matrix(,512,3)
+control_limits_row_Gabor[1:512,1] = mean_row_Gabor[1:512]-(3)*std_row_Gabor[1:512]
+control_limits_row_Gabor[1:512,2] = mean_row_Gabor[1:512]
+control_limits_row_Gabor[1:512,3] = mean_row_Gabor[1:512]+(3)*std_row_Gabor[1:512]
+for(i in 1:512){
+  for(k in 1:512){
+    if(imgGabor[i,k] < control_limits_row_Gabor[i,1] | imgGabor[i,k] > control_limits_row_Gabor[i,3] ){
+      img1[i,k] = 0;
+    } 
+  }
+}
+
+require(grDevices)
+op <- par(bg = "magenta")
+plot(c(100, 1200), c(100, 710), type = "n", xlab = "", ylab = "")
+rasterImage(imgActual, 100, 105, 612, 617, interpolate = FALSE)
+rasterImage(img1, 652, 105, 1164, 617, interpolate = FALSE)
+par(op)
---
+++ index.md
@@ -0,0 +1,41 @@
+# Statistical-Process-Control-Textile-Surfaces-Gabor-Filtering
+### Report
+[Here](files/1.report.html) Report.
+##### Image 2
+[Here](files/Image2.html) Result of Image 2.
+##### Image 3
+[Here](files/Image3.html) Result of Image 3.
+##### Image 4
+[Here](files/Image4.html) Result of Image 4.
+##### Image 5
+[Here](files/Image5.html) Result of Image 5.
+##### Image 6
+[Here](files/Image6.html) Result of Image 6.
+##### Image 7
+[Here](files/Image7.html) Result of Image 7.
+##### Image 8
+[Here](files/Image8.html) Result of Image 8.
+##### Image 9
+[Here](files/Image9.html) Result of Image 9.
+##### Image 10
+[Here](files/Image10.html) Result of Image 10.
+##### Image 11
+[Here](files/Image11.html) Result of Image 11.
+##### Image 12
+[Here](files/Image12.html) Result of Image 12.
+##### Image 13
+[Here](files/Image13.html) Result of Image 13.
+##### Image 14
+[Here](files/Image14.html) Result of Image 14.
+##### Image 15
+[Here](files/Image15.html) Result of Image 15.
+##### Image 16
+[Here](files/Image16.html) Result of Image 16.
+##### Image 17
+[Here](files/Image17.html) Result of Image 17.
+##### Image 18
+[Here](files/Image18.html) Result of Image 18.
+##### Image 19
+[Here](files/Image19.html) Result of Image 19.
+##### Image 20
+[Here](files/Image20.html) Result of Image 20.
